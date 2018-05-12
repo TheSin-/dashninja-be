6f4ef721c70db7cbd5d6fa4f60727154221edc6a
Justin F. Hallett <thesin@southofheaven.org>
2018-05-12 11:00:42 -0600

    Forgot operator

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $eventManager->attach('micro', function($event, $app) use ($mysqli) {
 
     // The server should have the TLS client certificate information and the remote peer address
     // If not, just fail early
-    if ((0 == 1 (!array_key_exists("VERIFIED",$_SERVER) || ($_SERVER['VERIFIED'] != "SUCCESS")
+    if ((0 == 1 && (!array_key_exists("VERIFIED",$_SERVER) || ($_SERVER['VERIFIED'] != "SUCCESS")
      || !array_key_exists("DN",$_SERVER) || (strlen($_SERVER['DN'])==0)))
      || !array_key_exists("REMOTE_ADDR",$_SERVER) || (strlen($_SERVER['REMOTE_ADDR'])==0)) {
       $response = new Phalcon\Http\Response();
